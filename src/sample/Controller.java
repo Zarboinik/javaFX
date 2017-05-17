ebbc40cc6a4ac57557dc54c7a58066f5f7070c3d
Sergey Kislitsyn <kislicin@gmail.com>
2017-05-17 14:43:13 +0700

    Update by time Label

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,13 +1,16 @@
 package sample;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.util.Duration;
 
 import java.text.SimpleDateFormat;
-import java.util.Date;
-
+import java.util.Calendar;
 
 public class Controller {
     @FXML
@@ -22,17 +25,58 @@ public class Controller {
     @FXML
     private Label label3;
 
-    private Date date = new Date();
-
-    private SimpleDateFormat formatForDateNow = new SimpleDateFormat("hh:mm:ss");
-
     @FXML
     public void initialize(){
         button.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
             button.setText("Спасибо!");
-            label1.setText(formatForDateNow.format(date));
-            label2.setText(formatForDateNow.format(date));
-            label3.setText(formatForDateNow.format(date));
+            bindToTimeForLabel1();
+            bindToTimeForLabel2();
+            bindToTimeForLabel3();
         });
+    }
+
+    private void bindToTimeForLabel1() {
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.millis(100),
+                        actionEvent -> {
+                            Calendar time = Calendar.getInstance();
+                            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
+                            label1.setText(simpleDateFormat.format(time.getTime()));
+                        }
+                ),
+                new KeyFrame(Duration.millis(100))
+        );
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
+    }
+
+    private void bindToTimeForLabel2() {
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.seconds(1),
+                        actionEvent -> {
+                            Calendar time = Calendar.getInstance();
+                            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
+                            label2.setText(simpleDateFormat.format(time.getTime()));
+                        }
+                ),
+                new KeyFrame(Duration.seconds(1))
+        );
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
+    }
+
+    private void bindToTimeForLabel3() {
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.seconds(10),
+                        actionEvent -> {
+                            Calendar time = Calendar.getInstance();
+                            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
+                            label3.setText(simpleDateFormat.format(time.getTime()));
+                        }
+                ),
+                new KeyFrame(Duration.seconds(10))
+        );
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
     }
 }
